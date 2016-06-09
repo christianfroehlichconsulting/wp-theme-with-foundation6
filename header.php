57176ad0891ed262b7e303ed36999fe84a6353b6
Tian Fröhlich <tianfroehlich@gmail.com>
2016-06-09 11:06:47 +0200

    update von wp-template eingearbeitet

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?> >
 	<head>
-		<meta charset="utf-8" />
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/img/favicon/favicon.ico" type="image/x-icon" />
